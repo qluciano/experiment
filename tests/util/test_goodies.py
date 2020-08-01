0e3475f8bc5f8bc474918115043856758ca98b0e
Luciano Quintabani <qluciano@gmail.com>
2020-08-01 10:40:13 -0300

    Refactor

--- tests/util/test_goodies.py
+++ tests/util/test_goodies.py
@@ -6,6 +6,6 @@ def test_fib_handles_base_cases():
     assert fib(1) == 1
 
 
-def test_fib_holds_recurrence_relation():
-    for n in range(2, 100):
+def test_fib_holds_recurrence_relation(bound=100):
+    for n in range(2, bound):
         assert fib(n) == fib(n - 1) + fib(n - 2)
